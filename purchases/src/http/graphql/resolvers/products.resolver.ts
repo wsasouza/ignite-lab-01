deaef56efb309b531403e2caf0ac474feee083b3
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-22 19:56:15 -0300

    Feat: Create route list all products

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,18 @@
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+
+import { ProductsService } from '../../../services/products.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+
+import { Product } from '../models/product';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  // @UseGuards(AuthorizationGuard)
+  products() {
+    return this.productsService.listAllProducts();
+  }
+}
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,7 +5,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+import { ProductsService } from '../services/products.service';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [ProductsResolver, ProductsService],
 })
 export class HttpModule {}
