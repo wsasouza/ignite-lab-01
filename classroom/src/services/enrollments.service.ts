7fbf05a36b5079d290aa4f07236a6500dc61fd4e
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-27 23:50:59 -0300

    Feat: Classroom - create services  and resolvers

---
+++ classroom/src/services/enrollments.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
+
+@Injectable()
+export class EnrollmentsService {
+  constructor(private prisma: PrismaService) {}
+
+  listAllEnrollments() {
+    return this.prisma.enrollment.findMany({
+      where: {
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  listEnrollmentsByStudent(studentId: string) {
+    return this.prisma.enrollment.findMany({
+      where: {
+        studentId,
+        canceledAt: null,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+}
---
+++ classroom/src/services/students.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma/prisma.service';
+
+@Injectable()
+export class StudentsService {
+  constructor(private prisma: PrismaService) {}
+
+  listAllStudents() {
+    return this.prisma.student.findMany();
+  }
+
+  getStudentById(id: string) {
+    return this.prisma.student.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+}
