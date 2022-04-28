b9b44c0ed9090ef65f6d6c94d096e2921704d60c
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-28 00:08:39 -0300

    Feat: Create course

--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
 import { PrismaService } from '../database/prisma/prisma.service';
 
+interface CreateCourseParams {
+  title: string;
+}
 @Injectable()
 export class CoursesService {
   constructor(private prisma: PrismaService) {}
@@ -13,6 +18,27 @@ export class CoursesService {
     return this.prisma.course.findUnique({
       where: {
         id,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseParams) {
+    const slug = slugify(title, { lower: true });
+
+    const courseAlreadyExists = await this.prisma.course.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (courseAlreadyExists) {
+      throw new Error('Course already exists');
+    }
+
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
       },
     });
   }
