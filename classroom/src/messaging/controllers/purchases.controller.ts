a258f25af8b0df90a036dfca33f745db1d27e53d
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-29 16:15:43 -0300

    Feat: Classroom - Finish microservice

--- classroom/src/messaging/controllers/purchases.controller.ts
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -1,6 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 
+import { CoursesService } from '../../services/courses.service';
+import { EnrollmentsService } from '../../services/enrollments.service';
+import { StudentsService } from '../../services/students.service';
+
 export interface Customer {
   authUserId: string;
 }
@@ -18,8 +22,37 @@ export interface PurchaseCreatedPayload {
 
 @Controller()
 export class PurchaseController {
+  constructor(
+    private studentsService: StudentsService,
+    private coursesService: CoursesService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
   @EventPattern('purchases.new-purchase')
   async puchaseCreated(@Payload('value') payload: PurchaseCreatedPayload) {
-    console.log(payload);
+    let student = await this.studentsService.getStudentByAuthUserId(
+      payload.customer.authUserId,
+    );
+
+    if (!student) {
+      student = await this.studentsService.createStudent({
+        authUserId: payload.customer.authUserId,
+      });
+    }
+
+    let course = await this.coursesService.getCourseBySlug(
+      payload.product.slug,
+    );
+
+    if (!course) {
+      course = await this.coursesService.createCourse({
+        title: payload.product.title,
+      });
+    }
+
+    await this.enrollmentsService.createEnrollment({
+      courseId: course.id,
+      studentId: student.id,
+    });
   }
 }
