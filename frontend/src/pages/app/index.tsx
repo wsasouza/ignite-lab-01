bf1f8b7d42c7098ff49bb01a25f0885e1ab86823
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-01 19:18:51 -0300

    Feat: Web - Configure Apollo Client and test first request

--- frontend/src/pages/app/index.tsx
+++ frontend/src/pages/app/index.tsx
@@ -1,8 +1,23 @@
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import {
+  getAccessToken,
+  useUser,
+  withPageAuthRequired,
+} from '@auth0/nextjs-auth0';
 import { GetServerSideProps } from 'next';
+import { gql, useQuery } from '@apollo/client';
+
+const PRODUCTS_QUERY = gql`
+  query getProducts {
+    products {
+      id
+      title
+    }
+  }
+`;
 
 export default function Home() {
   const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
 
   return (
     <>
@@ -10,8 +25,20 @@ export default function Home() {
         <h1>Hello Auth 0</h1>
         <pre>{JSON.stringify(user, null, 2)}</pre>
       </div>
+      <div>
+        <h1>Return Products</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+  getServerSideProps: async ({ req, res }) => {
+    console.log(getAccessToken(req, res));
+
+    return {
+      props: {},
+    };
+  },
+});
